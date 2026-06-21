2f945cd151754f967014166017c36ba07226e36e
Lucas <runyukio@gmail.com>
2026-06-21 15:09:07 -0300

    Corrige upload automatico CEC pelo launchd

--- scripts/download-cec-freshdesk.sh
+++ scripts/download-cec-freshdesk.sh
@@ -155,8 +155,7 @@ upload_cec_snapshot() {
         -X POST "$endpoint" \
         -H "Authorization: Bearer $token" \
         -H "Content-Type: application/json" \
-        --data-binary @- \
-        < "$JSON_FILE"
+        --data-binary @"$JSON_FILE"
     )"
     curl_status=$?
     set -e
